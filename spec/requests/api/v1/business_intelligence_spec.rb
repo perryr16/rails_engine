be3e78504dc1fc34a49a510a37d3430ccb62ccf5
Ross Perry <perryr16@gmail.com>
2020-07-11 18:07:06 -0600

    local testing for most rev and most items

--- spec/requests/api/v1/business_intelligence_spec.rb
+++ spec/requests/api/v1/business_intelligence_spec.rb
@@ -3,25 +3,40 @@ require 'rails_helper'
 
 
 describe 'business intelligence' do
-  it 'can get merchants with most revenue' do
+  before :each do 
+    FactoryBot.reload
     customer = create(:customer)
-    merchant1 = create(:merchant)
-    merchant2 = create(:merchant)
-    item = create(:item, merchant: merchant1)
+    @merchant1 = create(:merchant)
+    @merchant2 = create(:merchant)
+    @merchant3 = create(:merchant)
+    @merchant4 = create(:merchant)
 
-    invoice11 = create(:invoice, merchant: merchant1, customer: customer)
-    invoice12 = create(:invoice, merchant: merchant1, customer: customer)
-    invoice13 = create(:invoice, merchant: merchant1, customer: customer)
-    invoice21 = create(:invoice, merchant: merchant2, customer: customer)
-    invoice22 = create(:invoice, merchant: merchant2, customer: customer)
-    invoice23 = create(:invoice, merchant: merchant2, customer: customer)
+    item1 = create(:item, merchant: @merchant1)
+    item2 = create(:item, merchant: @merchant2)
+    item3 = create(:item, merchant: @merchant3)
+    item4 = create(:item, merchant: @merchant4)
 
-    invoice_item = create(:invoice_item, item: item, invoice: invoice11)
-    invoice_item = create(:invoice_item, item: item, invoice: invoice12)
-    invoice_item = create(:invoice_item, item: item, invoice: invoice13)
-    invoice_item = create(:invoice_item, item: item, invoice: invoice21)
-    invoice_item = create(:invoice_item, item: item, invoice: invoice22)
-    invoice_item = create(:invoice_item, item: item, invoice: invoice23)
+    invoice11 = create(:invoice, merchant: @merchant1, customer: customer)
+    invoice12 = create(:invoice, merchant: @merchant1, customer: customer)
+    invoice13 = create(:invoice, merchant: @merchant1, customer: customer)
+    invoice21 = create(:invoice, merchant: @merchant2, customer: customer)
+    invoice22 = create(:invoice, merchant: @merchant2, customer: customer)
+    invoice23 = create(:invoice, merchant: @merchant2, customer: customer)
+    invoice31 = create(:invoice, merchant: @merchant3, customer: customer)
+    invoice32 = create(:invoice, merchant: @merchant3, customer: customer)
+    invoice33 = create(:invoice, merchant: @merchant3, customer: customer)
+    invoice41 = create(:invoice, merchant: @merchant4, customer: customer)
+
+    invoice_item = create(:invoice_item, item: item1, invoice: invoice11, quantity: 100, unit_price: 0.01)
+    invoice_item = create(:invoice_item, item: item1, invoice: invoice12, quantity: 100, unit_price: 0.01)
+    invoice_item = create(:invoice_item, item: item1, invoice: invoice13, quantity: 100, unit_price: 0.01)
+    invoice_item = create(:invoice_item, item: item2, invoice: invoice21, quantity: 100, unit_price: 1000000)
+    invoice_item = create(:invoice_item, item: item2, invoice: invoice22, quantity: 1)
+    invoice_item = create(:invoice_item, item: item2, invoice: invoice23, quantity: 1)
+    invoice_item = create(:invoice_item, item: item3, invoice: invoice31, quantity: 100)
+    invoice_item = create(:invoice_item, item: item3, invoice: invoice32, quantity: 30)
+    invoice_item = create(:invoice_item, item: item3, invoice: invoice33, quantity: 10)
+    invoice_item = create(:invoice_item, item: item4, invoice: invoice41, quantity: 1, unit_price: 0.5)
 
     transaction = create(:transaction, invoice: invoice11, result: "success")
     transaction = create(:transaction, invoice: invoice12, result: "success")
@@ -29,38 +44,42 @@ describe 'business intelligence' do
     transaction = create(:transaction, invoice: invoice21, result: "success")
     transaction = create(:transaction, invoice: invoice22, result: "success")
     transaction = create(:transaction, invoice: invoice23, result: "failed")
+    transaction = create(:transaction, invoice: invoice31, result: "success")
+    transaction = create(:transaction, invoice: invoice32, result: "success")
+    transaction = create(:transaction, invoice: invoice33, result: "success")
+    transaction = create(:transaction, invoice: invoice41, result: "success")
+  end
+  it 'can get merchants with most revenue' do
+    
 
-    get "/api/v1/merchants/most_revenue?quantity=7"
+    get "/api/v1/merchants/most_revenue?quantity=3"
     json = JSON.parse(response.body, symbolize_names: true)
 
-    expect(json[:data].length).to eq(7)
+    expect(json[:data].length).to eq(3)
 
-    expect(json[:data][0][:attributes][:name]).to eq("Dicki-Bednar")
-    expect(json[:data][0][:id]).to eq("14")
+    expect(json[:data][0][:attributes][:name]).to eq(@merchant2.name)
+    expect(json[:data][0][:id]).to eq("2")
 
-    expect(json[:data][3][:attributes][:name]).to eq("Bechtelar, Jones and Stokes")
-    expect(json[:data][3][:id]).to eq("10")
+    expect(json[:data][1][:attributes][:name]).to eq(@merchant3.name)
+    expect(json[:data][1][:id]).to eq("3")
 
-    expect(json[:data][6][:attributes][:name]).to eq("Rath, Gleason and Spencer")
-    expect(json[:data][6][:id]).to eq("53")
+    expect(json[:data][2][:attributes][:name]).to eq(@merchant1.name)
+    expect(json[:data][2][:id]).to eq("1")
   end
 
   it 'can get merchants who have sold the most items' do
-    get "/api/v1/merchants/most_items?quantity=8"
+    get "/api/v1/merchants/most_items?quantity=2"
 
     json = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
 
-    expect(json[:data].length).to eq(8)
 
-    expect(json[:data][0][:attributes][:name]).to eq("Kassulke, O'Hara and Quitzon")
-    expect(json[:data][0][:id]).to eq("89")
+    expect(json[:data].length).to eq(2)
 
-    expect(json[:data][3][:attributes][:name]).to eq("Okuneva, Prohaska and Rolfson")
-    expect(json[:data][3][:id]).to eq("98")
+    expect(json[:data][0][:attributes][:name]).to eq(@merchant1.name)
+    expect(json[:data][0][:id]).to eq("1")
 
-    expect(json[:data][7][:attributes][:name]).to eq("Terry-Moore")
-    expect(json[:data][7][:id]).to eq("84")
+    expect(json[:data][1][:attributes][:name]).to eq(@merchant3.name)
+    expect(json[:data][1][:id]).to eq("3")
   end
 
   xit 'can get revenue between two dates' do
